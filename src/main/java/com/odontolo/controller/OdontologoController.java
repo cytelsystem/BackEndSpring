6ef573e477550cafd61d1c9c5aa01c0e99119e0d
Hector Javier Moreno <cytelsystem@gmail.com>
2022-06-09 10:00:10 -0500

    odontologos funcionando desde la raiz

--- src/main/java/com/odontolo/controller/OdontologoController.java
+++ src/main/java/com/odontolo/controller/OdontologoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("odontologos")
+@RequestMapping("/")
 public class OdontologoController {
 
     private final OdontologoService odontologoService;

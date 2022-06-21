9db53e4074d520558463af2ea2dae659562fdc8a
Hector Javier Moreno <cytelsystem@gmail.com>
2022-06-21 14:10:22 -0500

    hibernateEscuelaIniciando

---
+++ src/test/java/com/dh/escuela/EscuelaApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dh.escuela;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EscuelaApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
--- src/test/java/com/dh/storage/StorageApplicationTest.java
+++
@@ -1,7 +0,0 @@
-package com.dh.storage;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class StorageApplicationTest {
-
-}
